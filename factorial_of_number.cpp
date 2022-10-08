426f18edda7d8834514573e9c07e1d77a8c93084
Chandan <109679579+I-am-CHANDAN@users.noreply.github.com>
2022-10-08 11:24:14 +0530

    create factorial of number

---
+++ factorial_of_number.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+// create a function
+int factorial(int n)
+{
+    // check the condition
+    if (n <= 1)
+    {
+        return 1;
+    }
+
+    // otherwise
+    else
+    {
+        return n * factorial(n - 1);
+    }
+}
+
+// driver code
+int main()
+{
+    int n;
+    cout << "Enter a number for factorial :" << endl;
+    cin >> n;
+
+    // call the function
+    cout << "The Factorial of " << n << " is " << factorial(n) << endl;
+
+    return 0;
+}
