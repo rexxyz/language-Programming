7dabc117c77c9593987b7f0860ef1f6dd892dd05
RandeepKaur16 <118152763+RandeepKaur16@users.noreply.github.com>
2022-11-18 11:17:50 +0530

    Create SetMatrixRowsAndColumnToZero.java
    [Matrix operations] | Add | Given an m x n matrix of 0s and 1s, if an element is 0, set its entire row and column to 0

---
+++ java/SetMatrixRowsAndColumnToZero.java
@@ -0,0 +1,175 @@
+package java;
+
+import java.util.ArrayList;
+
+/*
+ * PROBLEM
+ Given an m x n matrix of 0s and 1s, if an element is 0, set its entire row and column to 0.
+
+Do it in place.
+
+Example
+
+Given array A as
+
+1 0 1
+1 1 1
+1 1 1
+On returning, the array A should be :
+
+0 0 0
+1 0 1
+1 0 1
+
+SOLUTION:------
+SCAN ALL ELEMENTS OF ARRAY => MAIN SCAN
+WITH A CELL HAVING A ZERO, MARK CORRESPONDING ROW WITH -1,
+	EXCEPT FOR CELLS IN THAT ROW WHICH HAVE A ZERO -
+		IF WE MAKE ALL ZEROES IN THAT ROW ALSO -1, WHEN WE REACH THAT CELL IN OUR MAIN SCAN,
+		WE WOULD HAVE ALREADY LOST THE ORIGINAL ZERO ELEMENT CELL AS IT IS NOW -1
+MARK CORRESPONDING COLUMN WITH -1, EXCEPT FOR CELLS WHICH HAVE A ZERO(FOR SAME LOGIC AS FOR ROW)
+REPEAT FOR ALL ELEMENTS IN ARRAY
+AT THE END, SCAN ALL ELEMENTS OF ARRAY ONE MORE TIME AND CHANGE ALL -1 CELL ELEMENTS TO 0
+ */
+public class SetMatrixRowsAndColumnToZero {
+
+    public static void main(String[] args) {
+        // Using Integer Array
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        int [][] inputArray = {{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+        };
+        System.out.println("\n\nMatrix after operation: \n" + setMatrixZeroesWithIntArray(inputArray));
+
+        // Using ArrayList
+        ArrayList<Integer> a1 = new ArrayList<>();
+		a1.add(0);
+		a1.add(0);
+		list.add(a1);
+		ArrayList<Integer> a2 = new ArrayList<>();
+		a2.add(1);
+		a2.add(5);
+		list.add(a2);
+		System.out.println("Matrix before operation: \n" + list);
+		System.out.println("Matrix after operation: \n" + setMatrixZeroesWithArrayList(list));
+    }
+
+    /*WITH ARRAYLIST*/
+    private static ArrayList<ArrayList<Integer>> setMatrixZeroesWithArrayList(
+            ArrayList<ArrayList<Integer>> list) {
+        int columns = list.get(0).size(); // columns
+        int rows = list.size(); // rows
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                if (0 == list.get(i).get(j))
+                {
+                    // SCAN ALL ELEMENTS OF ARRAY => MAIN SCAN
+                    // WITH A CELL HAVING A ZERO, MARK CORRESPONDING ROW WITH -1,
+                    // 		EXCEPT FOR CELLS IN THAT ROW WHICH HAVE A ZERO -
+                    // 			IF WE MAKE ALL ZEROES IN THAT ROW ALSO -1, WHEN WE REACH THAT CELL IN OUR MAIN SCAN,
+                    //			WE WOULD HAVE ALREADY LOST THE ORIGINAL ZERO ELEMENT CELL AS IT IS NOW -1
+                    for (int jj = 0; jj < columns; jj++) // scan for this row
+                    {
+                        if (list.get(i).get(jj) != 0)
+                            list.get(i).set(jj, -1);
+                    }
+
+                    System.out.println("\n -----------------------------------\n" + list);
+                    System.out.println("\n -----------------------------------\n");
+                    // MARK CORRESPONDING COLUMN WITH -1, EXCEPT FOR CELLS WHICH HAVE A ZERO
+                    // FOR SAME LOGIC AS ABOVE
+                    for (int ii = 0; ii < rows; ii++)	// scan for this column
+                    {
+                        if (list.get(ii).get(j) != 0)
+                            list.get(ii).set(j, -1);
+                    }
+                }
+            }
+        }
+
+        // CHANGE ALL -1 CELL ELEMENTS TO 0
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                if (-1 == list.get(i).get(j))
+                    list.get(i).set(j, 0);
+            }
+        }
+        return list;
+    }
+
+    /*WITH INT ARRAY*/
+    private static int[][] setMatrixZeroesWithIntArray(
+            int[][] list) {
+        int columns = list[0].length; // columns
+        int rows = list.length; // rows
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                if (0 == list[i][j])
+                {
+                    for (int jj = 0; jj < columns; jj++) // scan for this row
+                    {
+                        if (list[i][jj] != 0)
+                            list[i][jj]= -1;
+                    }
+
+                    for (int ii = 0; ii < rows; ii++)	// scan for this column
+                    {
+                        if (list[ii][j] != 0)
+                            list[ii][j] = -1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                if (-1 == list[i][j])
+                    list[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < rows; i++)
+        {
+            System.out.println();
+            for (int j = 0; j < columns; j++)
+            {
+                System.out.print(list[i][j]);
+            }
+        }
+        return list;
+    }
+}
